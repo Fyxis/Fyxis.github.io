4e7d9bbe23e2e3b08553572bde4ac09da8bed998
Fyxis <bagusadrianus@gmail.com>
2024-10-26 09:12:45 +0700

    added all

---
+++ assets/js/scriptFunction.js
@@ -0,0 +1,13 @@
+var typed = new Typed("#job", {
+	strings: ["Backend Developer", "UI/UX Designer", "Programmer Enthusiast"],
+	typeSpeed: 80,
+	backSpeed: 50,
+	backDelay: 2200,
+	loop: true,
+});
+
+AOS.init({
+	throttleDelay: 99,
+	delay: 0,
+	duration: 1000,
+});
