e684b9ce5f242d55cd5c4f7151332827ac0b6302
Fyxis <bagusadrianus@gmail.com>
2024-12-30 11:15:54 +0700

    updated portfolio

---
+++ assets/js/projects.js
@@ -0,0 +1,30 @@
+const projects = [
+    {
+        "id_project": 1,
+        "title": "Web Profile SMK Negeri 3 Kendal",
+        "image": "assets/image/web-profile-sekolah.webp",
+        "description": "This is official web profile for SMK Negeri 3 Kendal, designed to provide comprehensive information about the school, programs, activities, and student achievements.",
+        "site_link": "https://smkn3kendal.sch.id/2024"
+    },
+    {
+        "id_project": 2,
+        "title": "AJS Cooperative Web",
+        "image": "assets/image/web-koperasi-sekolah.webp",
+        "description": "A project to develop a cooperative web application that facilitates member registration, financial management, and transaction records efficiently.",
+        "site_link": "https://koperasi.smkn3kendal.sch.id"
+    },
+    {
+        "id_project": 3,
+        "title": "Limitless RPG Discord Bot",
+        "image": "assets/image/discord-bot.png",
+        "description": "Developed a Discord bot named Limitless RPG, featuring engaging RPG mechanics such as quests, battles, and item collection, providing an interactive gaming experience for users.",
+        "site_link": "unknown"
+    },
+    {
+        "id_project": 4,
+        "title": "Website Project List",
+        "image": "assets/image/web-project-list.png",
+        "description": "A project listing website is a simple online platform dedicated to showcasing completed or ongoing projects. It typically features a clean layout with project titles, brief descriptions, and links or visuals for each item.",
+        "site_link": "https://fyxis.github.io"
+    }
+]
--- assets/js/scriptFunction.js
+++ assets/js/scriptFunction.js
@@ -1,13 +1,58 @@
 var typed = new Typed("#job", {
-	strings: ["Backend Developer", "UI/UX Designer", "Programmer Enthusiast"],
-	typeSpeed: 80,
-	backSpeed: 50,
-	backDelay: 2200,
-	loop: true,
+    strings: ["Backend Developer", "UI/UX Designer", "Programmer Enthusiast"],
+    typeSpeed: 80,
+    backSpeed: 50,
+    backDelay: 2200,
+    loop: true,
 });
 
 AOS.init({
-	throttleDelay: 99,
-	delay: 0,
-	duration: 1000,
+    throttleDelay: 99,
+    delay: 0,
+    duration: 1000,
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+    const projectContainer = document.getElementById("projects-list");
+    projects.forEach((project) => {
+        const projectCard = document.createElement("div");
+        projectCard.className = `project-card`;
+
+        projectCard.innerHTML = `
+			<div class="max-w-sm rounded overflow-hidden shadow-lg">
+				<img class="w-full" src=${project.image} alt="${project.image}" />
+				<div class="px-6 py-4">
+					<div class="font-bold text-xl mb-2">${project.title}</div>
+					<p class="text-gray-500 text-base">
+						${project.description}
+					</p>
+				</div>
+			</div>
+		`;
+        projectContainer.appendChild(projectCard);
+
+        projectCard.addEventListener("mouseenter", () => {
+            projectCard.classList.add("scale");
+            Array.from(projectContainer.children).forEach((card) => {
+                if (card !== projectCard) card.classList.add("blur");
+            });
+        });
+
+        projectCard.addEventListener("mouseleave", () => {
+            projectCard.classList.remove("scale");
+            Array.from(projectContainer.children).forEach((card) =>
+                card.classList.remove("blur")
+            );
+        });
+
+        projectCard.addEventListener("click", () => {
+            if (project.site_link) {
+                window.open(project.site_link, "_blank").focus();
+            } else {
+                console.warn(
+                    `No site link available for project: ${project.title}`
+                );
+            }
+        });
+    });
 });
